52e1d7613fe8f0d54009eddf6ca5a243e0532211
Yifei Liu <34564223+EzekielDaun@users.noreply.github.com>
2024-03-02 21:55:08 -0800

    Serial protocol WIP TODO: * set_row_pos * set_led_pwr * parse response

--- microplate_reader/window/mr_main_window.py
+++ microplate_reader/window/mr_main_window.py
@@ -4,8 +4,11 @@ from PySide6.QtGui import QTextCursor
 from PySide6.QtSerialPort import QSerialPort
 from PySide6.QtWidgets import (
     QDockWidget,
+    QHBoxLayout,
     QMainWindow,
     QMenuBar,
+    QPushButton,
+    QSpinBox,
     QTableWidget,
     QTextBrowser,
     QVBoxLayout,
@@ -20,6 +23,8 @@ class MRMainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+        self.__serial_port: QSerialPort = None  # type: ignore
 
         # Window Title
         self.setWindowTitle("Microplate Reader")
@@ -45,6 +50,7 @@ class MRMainWindow(QMainWindow):
 
         # Central Widget
         self.__central_widget = MR_main_window_central_widget()
+        self.__central_widget.signal_serial_send.connect(self.__slot_comport_send)
         self.setCentralWidget(self.__central_widget)
 
     # Slots for Redirect stdout and stderr
@@ -67,18 +73,49 @@ class MRMainWindow(QMainWindow):
         self.__serial_port.readyRead.connect(self.__slot_comport_ready_read)
 
     def __slot_comport_ready_read(self):
-        logger.info(self.__serial_port.readAll())
+        while self.__serial_port.canReadLine():
+            logger.info(self.__serial_port.readLine().toStdString())
+
+    def __slot_comport_send(self, data: str):
+        data = "/" + data + "\n"
+        logger.info(f"Comport send:\n{data}")
+        if not self.__serial_port:
+            logger.error("Comport not connected")
+            return
+        self.__serial_port.write(data.encode("ascii"))
 
 
 class MR_main_window_central_widget(QWidget):
+    signal_serial_send = Signal(str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.__table_widget = QTableWidget()
+        self.__home_button = QPushButton("Home")
+        self.__home_button.clicked.connect(self.__slot_home_button_clicked)
+        self.__read_button = QPushButton("Read")
+        self.__read_button.clicked.connect(self.__slot_read_button_clicked)
+        self.__read_all_button = QPushButton("Read All")
+        self.__read_all_button.clicked.connect(self.__slot_read_all_button_clicked)
 
-        # 8 columns and 12 rows
-        self.__table_widget.setColumnCount(8)
-        self.__table_widget.setRowCount(12)
+        # Debug Utilities
+        self.__move_abs_spinbox = QSpinBox()
+        self.__move_abs_spinbox.setMinimum(-(2**31))
+        self.__move_abs_spinbox.setMaximum(2**31 - 1)
+        self.__move_abs_spinbox.setValue(0)
+
+        self.__move_abs_button = QPushButton("Move Absolute")
+
+        self.__top_layout = QHBoxLayout()
+        self.__top_layout.addWidget(self.__home_button)
+        self.__top_layout.addWidget(self.__read_button)
+        self.__top_layout.addWidget(self.__read_all_button)
+
+        self.__top_layout.addWidget(self.__move_abs_spinbox)
+        self.__top_layout.addWidget(self.__move_abs_button)
+        self.__move_abs_button.clicked.connect(self.__slot_move_abs_button_clicked)
+
+        self.__table_widget = QTableWidget(12, 8)
 
         self.__table_widget.setHorizontalHeaderLabels(
             [str(chr(ord("A") + i)) for i in range(self.__table_widget.columnCount())]
@@ -93,7 +130,25 @@ class MR_main_window_central_widget(QWidget):
         )
 
         self.setLayout(QVBoxLayout())
+        self.layout().addLayout(self.__top_layout)  # type: ignore
         self.layout().addWidget(self.__table_widget)
+
+    def __slot_home_button_clicked(self):
+        logger.info("Home button clicked")
+        self.signal_serial_send.emit("home")
+
+    def __slot_read_button_clicked(self):
+        logger.info("Read button clicked")
+        for item in self.__table_widget.selectedIndexes():
+            self.signal_serial_send.emit(f"scan_well {item.row()} {item.column()}")
+
+    def __slot_read_all_button_clicked(self):
+        logger.info("Read all button clicked")
+        self.signal_serial_send.emit("scan_all")
+
+    def __slot_move_abs_button_clicked(self):
+        logger.info("Move absolute button clicked")
+        self.signal_serial_send.emit(f"move_abs {self.__move_abs_spinbox.value()}")
 
     def __slot_cell_clicked(self, row: int, col: int):
         cell_text = (
